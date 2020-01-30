f85d6eb76a0f2eaeaa0057158de7b06e02996846
s.t. Chen <53397044+st-cccp@users.noreply.github.com>
2020-01-30 19:29:40 +0800

    Update The_script.cpp

--- The_script.cpp
+++ The_script.cpp
@@ -1,4 +1,4 @@
-﻿// The_script.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// The_script.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 
@@ -12,42 +12,49 @@ using namespace cv;
 #include "Stiprocess.h"
 //Homography类参考 https://blog.csdn.net/czl389/article/details/60325970 ，将做出局部修改
 
+DenoteTime denotetime;
+bool stop = false;
 
 int main()
 {
+	denotetime();
 	int total_cameras = 2;//相机总数
 	VideoCapture* videos = new VideoCapture[total_cameras];//视频源
 	{
 		string* names = new string[total_cameras];
+		names[0] = "http://admin:admin@192.168.31.202:8081";
+		names[1] = "http://admin:admin@192.168.31.15:8081";
 		
 		for (int i = 0; i < total_cameras; i++)
 		{
 			Openvideo(videos[i], names[i]);
 		}
 	}
-//打开视频阶段，视频源生成为videos[i]，默认以total_cameras/2下表位置视频源的为参考进行拼接，输入保证按空间顺序连续输入
+	denotetime();//打开视频阶段，视频源生成为videos[i]，默认以total_cameras/2下表位置视频源的为参考进行拼接，输入保证按空间顺序连续输入
 	
 	MappingsCenter mapcenter(total_cameras, videos);//图像间映射管理中心
+	while(!stop)
 	{
 		mapcenter.calculate();//计算可能有的所有单应性矩阵
+		mapcenter.drawmatches(); if (waitKey(25) > 0)stop = true;
 	}
-//求解单应性矩阵阶段，使用MappingsCenter类集中管理，计算
+	denotetime();//求解单应性矩阵阶段，使用MappingsCenter类集中管理，计算
 
 	namedWindow("Result");
 
 	Stitchingcontroler sticontroler(total_cameras, videos);
-	bool stop = 0;
+	bool stop = false;
 	while (!stop)
 	{
 		sticontroler.Catchimgs();
 		//默认直接截取，考虑到同时性问题，可能需要后续增加缓冲区等保证拼到一起的为同时刻の图像
 		sticontroler.Changebright();
 		//根据中间参考图像（或者综合考虑？如果时间复杂度允许）调整各张图像的整体亮度
-		imshow("Result", sticontroler.stitch());
+		//imshow("Result", sticontroler.stitch());
 		if (cv::waitKey(25) > 0)
 			stop = true;
 	}
-	//实时拼接阶段，分步骤进行，首先改亮度（也可改下对比度）
+	denotetime();//实时拼接阶段，分步骤进行，首先改亮度（也可改下对比度）
 
 }
 
