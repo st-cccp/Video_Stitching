ff699818dd3440036621c8e774113bbf87ee5923
s.t. Chen <53397044+st-cccp@users.noreply.github.com>
2020-02-05 19:46:11 +0800

    Update Homography.h

--- Homography.h
+++ Homography.h
@@ -10,18 +10,17 @@ using namespace cv;
 class Homography
 {
 private:
-	Mat img1;
-	Mat img2;
+	Mat img[2];
+
+	Mat imggray[2];
 
 	Ptr<FeatureDetector> detector;
 	Ptr<DescriptorExtractor> extractor;
 	Ptr<DescriptorMatcher> matcher;
 
-	vector<KeyPoint> keyPoints1;
-	vector<KeyPoint> keyPoints2;
+	vector<KeyPoint> keyPoints[2];
 
-	Mat descriptors1;
-	Mat descriptors2;
+	Mat descriptors[2];
 
 	vector<DMatch> firstMatches;
 	vector<DMatch> matches;
@@ -42,6 +41,7 @@ public:
 	void setFeatureDetector(string detectorName);
 	void setDescriptorExtractor(string descriptorName);
 	void setDescriptorMatcher(string matcherName);
+	void clearresult();
 
 	vector<KeyPoint> getKeyPoints1();
 	vector<KeyPoint> getKeyPoints2();
@@ -63,8 +63,14 @@ private:
 	void matchesToSelfPoints();
 	void findHomography();
 	void matchesFilter();
+	
+	bool checkmatch(DMatch m);
+	bool colorcheck(DMatch m);
+	void Changebright();
+	void ChangeKeypoint(int bax0, int bay0, KeyPoint& kp);
+	void detectKeyPointsHere(Rect area, int subscript);
+	void detectKeyPointsBlocks(int index = 0);
 /*	————————————————
 		版权声明：本文为CSDN博主「czl389」的原创文章，遵循 CC 4.0 BY - SA 版权协议，转载请附上原文出处链接及本声明。
 		原文链接：https ://blog.csdn.net/czl389/article/details/60325970*/
 };
-
